f581ec67393b3d1461a0a85801dbe1119e076346
adventure-star <Boristolstyakov@protonmail.com>
2021-01-19 14:02:12 +0700

    initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="bg-gray-200 flex h-screen items-center justify-center">
+    <h1 class="text-3xl border-t-2 border-b-2 border-gray-400">
+      Vue CLI 3 (router, Vuex, linting, SCSS) + TailwindCSS + ESLint Prettier
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home"
+};
+</script>
+
+<style lang="scss" scoped></style>
